53618189b52202328c82dbc7d490121886cab2d1
Ananda Putra Perdana <anandaputraperdana5@gmail.com>
2021-10-28 19:07:07 +0700

    Update VIEW_SURAT_LANJUTAN.php

--- VIEW_SURAT_LANJUTAN.php
+++ VIEW_SURAT_LANJUTAN.php
@@ -1,9 +1,10 @@
 <?php
+error_reporting(0);
 $con = new mysqli("localhost", "root", "", "db_surat_ananda");
 
 $tgl = date('d F Y');
 
-$query = mysqli_query($con, 'SELECT * FROM tbl_surat ')
+$query = mysqli_query($con, 'SELECT * FROM tbl_surat ');
 
 ?>
 <!DOCTYPE html>
@@ -14,8 +15,39 @@ $query = mysqli_query($con, 'SELECT * FROM tbl_surat ')
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </head>
 <body>
+    <?php
+    $pesan = $_GET['pesan'];
+    $frm = $_GET['frm'];
+
+    if($pesan =='success' && $frm =='del') {
+
+    ?>
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+    <strong>Selamat!!!</strong> Anda Selesai Menghapus data
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    <?php
+    } else if($pesan=='success' && $frm =='add') {
+    ?>
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+    <strong>Selamat!!!</strong> Anda Selesai Menambahkan data
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    <?php
+    } else if($pesan=='success' && $frm =='edit') {
+    ?>
+    <div class="alert alert-primary alert-dismissible fade show" role="alert">
+    <strong>Selamat!!!</strong> Anda Selesai mengubah data
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    <?php
+    }
+    ?>
+    
+    
     <h1 style="text-align:center"><b>LIST SURAT</b></h1>
     <table class="table">
+    <a href="ADD_SURAT_LANJUTAN.php">TAMBAH</a>
         <thead class="table-dark">
             <td><b>NO SURAT</b></td>
             <td><b>JENIS SURAT</b></td>
@@ -46,14 +78,47 @@ $query = mysqli_query($con, 'SELECT * FROM tbl_surat ')
 		<td><?php echo $isi['ttd_surat'];?></td>
         <td><?php echo $isi['ttd_mengetahui'];?></td>
         <td><?php echo $isi['ttd_menyetujui'];?></td>
-        <td>EDIT</td>
-        <td>DELETE</td>
+        <td><a href="EDIT_SURAT_LANJUTAN.php?id=<?php echo $isi['id'];?>">EDIT</a></td>
+        <td><a href="#" data-bs-toggle="modal" data-bs-target="#deletesurat<?php echo $isi['id'];?>">DELETE</a></td>
 	</tr>
+
+    <div class="example-modal">
+        <div id="deletesurat<?php echo $isi['id'];?>" class="modal fade" role="dialog" style="display:none;">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                <form class="row g-3" method="post" action="DELETE.php" name="tambahh" name="form1">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-bs-dismiss="modal" aria-label="close"><span aria-hidden="true">&times;</span></button>
+                    <h3 class="modal-title">Konfirmasi Delete data Surat</h3>
+                    </div>
+                    <div class="modal-body">
+                        <input type="hidden" id="id" name="id" value="<?php echo $isi['id'];?>">
+                        <h4 class="text-center">Apakah Anda Yakin Ingin Menghapus No Surat <?php echo $isi['no_surat'];?><strong><span class="grt"></span></strong></h4>
+                    </div>
+                    <div class="modal-footer">
+                        <button id="nodelete" type="button" class="btn btn-danger pull-left" data-bs-dismiss="modal">Cancel</button>
+                        <button type="submit" class="btn btn-primary" name="delete" id="delete">Delete</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <?php
+
     }
     ?>
     </tbody>
     </table>
+    <?php
+
+     if(isset($_POST['update'])) {
+         $id = $_POST['id'];
+
+         $result = mysqli_query($con, "DELETE FROM `tbl_surat` WHERE `tbl_surat`.`id` = $id");
+        
+      }
+    ?>
 </body>
 <script src="..assets/js/bootstrap.bundle.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
